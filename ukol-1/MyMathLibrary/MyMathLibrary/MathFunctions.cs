c0f798a7f5bfa0e8183d9f0a675f34957a2a1761
Pauline <takethejess@gmail.com>
2025-03-19 20:53:12 +0100

    feat(MyMathLibrary): library files added for MathApp

---
+++ ukol-1/MyMathLibrary/MyMathLibrary/MathFunctions.cs
@@ -0,0 +1,27 @@
+﻿namespace MyMathLibrary
+{
+    public class MathFunctions
+    {
+        /// <summary>
+        /// Vypočítá faktoriál zadaného čísla.
+        /// </summary>
+        /// <param name="n">Číslo, pro které se počítá faktoriál.</param>
+        /// <returns>Faktoriál zadaného čísla.</returns>
+        /// <exception cref="ArgumentException">Vyvolá výjimku, pokud je vstupní číslo záporné.</exception>
+        public static long Factorial(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("Faktoriál je definován pouze pro nezáporná čísla.");
+            }
+
+            long result = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
+
