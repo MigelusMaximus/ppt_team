44c1bb25aaf58a265faf2aa45725ec1867c00ea3
Pauline <takethejess@gmail.com>
2025-03-19 20:47:56 +0100

    feat(obdelnik12):Application_12 files added

---
+++ ukol-1/Application_12/Application_12/Zavislosti.h
@@ -0,0 +1,6 @@
+#ifndef ZAVISLOSTI_H
+#define ZAVISLOSTI_H
+
+void zobraz_zavislosti();  // Déclaration de la fonction
+
+#endif
---
+++ ukol-1/Application_12/Application_12/pch.cpp
@@ -0,0 +1,5 @@
+﻿// pch.cpp: zdrojovy soubor odpovidajici predkompilovane hlavicce; nezbytny k tomu, aby kompilace byla uspesna
+
+#include "pch.h"
+
+// Tento soubor lze obvykle ignorovat, ale pokud pouzivate predkompilovane hlavicky, ulozte si ho.
