60d25d473590b6738671d17e43a09f2c3546f2c3
Pauline <takethejess@gmail.com>
2025-04-21 18:37:07 +0200

    init: init repo for ukol 6

---
+++ src/main/java/org/example/fakturatrida/FakturaTridaApplication.java
@@ -0,0 +1,13 @@
+package org.example.fakturatrida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakturaTridaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakturaTridaApplication.class, args);
+    }
+
+}
